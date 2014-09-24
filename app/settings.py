db3300f911372c26374c3c127680090ec0ee331b
Max Tepkeev <m.tepkeev@ailove.ru>
2014-09-24 20:58:09 +0400

    test runner to make check pass

--- app/settings.py
+++ app/settings.py
@@ -196,6 +196,9 @@ INSTALLED_APPS = (
     'django.contrib.admin'
 )
 
+# Test Runner
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
