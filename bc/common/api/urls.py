e78ca9ee5031394a6b0552712c8404503e257d80
Alex Shalynin <a.shalynin@ailove.ru>
2019-06-24 22:04:35 +0300

    Add method to retrieve program sources

--- bc/common/api/urls.py
+++ bc/common/api/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 app_name = "common"
 
-urlpatterns = [path("areas", views.AreaListAPIView.as_view(), name="list")]
+urlpatterns = [
+    path("areas", views.AreaListAPIView.as_view(), name="areas"),
+    path(
+        "program-sources",
+        views.ProgramSourceListAPIView.as_view(),
+        name="program_sources",
+    ),
+]
--- bc/common/api/views.py
+++ bc/common/api/views.py
@@ -2,8 +2,14 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from bc.services.bc_v1.v3.api import BusinessClassAPI
+from bc.services.bc_v1.v3.constants import PROGRAM_SOURCE_CHOICES
 
 
 class AreaListAPIView(APIView):
     def get(self, request, *args, **kwargs):
         return Response(BusinessClassAPI.areas())
+
+
+class ProgramSourceListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(PROGRAM_SOURCE_CHOICES)
